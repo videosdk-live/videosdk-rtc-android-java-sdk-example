29d9bd2a20b941b057cd0c712c41ee587411a22a
Isha <ishabodiwala@gmail.com>
2022-01-12 12:18:55 +0530

    Remove Auth Api Server Dependencies

--- app/src/main/java/live/videosdk/rtc/android/java/JoinActivity.java
+++ app/src/main/java/live/videosdk/rtc/android/java/JoinActivity.java
@@ -2,6 +2,7 @@ package live.videosdk.rtc.android.java;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,7 +19,8 @@ import org.json.JSONObject;
 
 public class JoinActivity extends AppCompatActivity {
 
-    private final String Token = BuildConfig.TOKEN;
+    private final String AUTH_TOKEN = BuildConfig.AUTH_TOKEN;
+    private final String AUTH_URL= BuildConfig.AUTH_URL;
 
     private EditText etMeetingId;
 
@@ -32,19 +34,78 @@ public class JoinActivity extends AppCompatActivity {
         etMeetingId = findViewById(R.id.etMeetingId);
 
         btnCreate.setOnClickListener(v -> {
-            createMeeting(Token);
+            getToken(null);
         });
 
         btnJoin.setOnClickListener(v -> {
             String meetingId = etMeetingId.getText().toString();
-            joinMeeting(Token, meetingId);
+            if("".equals(meetingId))
+            {
+                Toast.makeText(JoinActivity.this, "Please Enter Meeting Id", Toast.LENGTH_SHORT).show();
+            }else {
+                getToken(meetingId);
+            }
         });
 
     }
 
+    private boolean isNullOrEmpty(String str){
+        return "null".equals(str) || "".equals(str) || null==str;
+    }
+
+    private void getToken(@Nullable String meetingId) {
+        if(!isNullOrEmpty(AUTH_TOKEN) && !isNullOrEmpty(AUTH_URL) )
+        {
+            Toast.makeText(JoinActivity.this, "Please Provide only one - either auth_token or auth_url", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            if(!isNullOrEmpty(AUTH_URL)) {
+               AndroidNetworking.get(AUTH_URL + "/get-token")
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String token = response.getString("token");
+                                    if (meetingId == null) {
+                                        createMeeting(token);
+                                    } else {
+                                       joinMeeting(token, meetingId);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                anError.printStackTrace();
+                                Toast.makeText(JoinActivity.this, anError.getErrorDetail(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }else if(!isNullOrEmpty(AUTH_TOKEN))
+            {
+                try {
+                    if (meetingId == null) {
+                        createMeeting(AUTH_TOKEN);
+                    } else {
+                        joinMeeting(AUTH_TOKEN, meetingId);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else
+            {
+                Toast.makeText(JoinActivity.this, "Please Provide auth_token or auth_url", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+
     private void createMeeting(String token) {
         AndroidNetworking.post("https://api.zujonow.com/api/meetings")
-                .addHeaders("Content-Type", "application/json")
                 .addHeaders("Authorization",token)
                 .build().getAsJSONObject(new JSONObjectRequestListener() {
             @Override
